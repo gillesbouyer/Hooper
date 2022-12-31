4d2932dd4adcc77f79a32e9de2ff28db1dda165c
gillesbouyer <gilles.bouyer@gmail.com>
2022-12-30 19:37:01 -0600

    sync workweek3

---
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import transformers
+import torch
+from transformers import pipeline
+#    pipeline("translation_en_to_fr", model = "fast_api_tutorial/app/model/gilles1")
+traduction = pipeline("translation_en_to_fr", model = "model/gilles1")
+
+app = FastAPI()
+
+class TextToTranslate(BaseModel):
+    input_text: str
+
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello toto"}
+    
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+@app.post("/echo")
+def echo(text_to_translate: TextToTranslate):
+#    return {"message": text_to_translate.input_text}#
+    return {"message":traduction(text_to_translate.input_text)}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app",
+     host="0.0.0.0", port=8000, reload=True)
