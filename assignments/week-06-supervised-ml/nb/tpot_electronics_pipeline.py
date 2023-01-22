1bf2c7c25ba4c21ed2b8bbd00bd55927f655a122
gillesbouyer <gilles.bouyer@gmail.com>
2023-01-22 15:50:19 -0600

    update questions4,5 work6

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.feature_selection import VarianceThreshold
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import LinearSVC
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -13,11 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 1.0
+# Average CV score on the training set was: 0.9925925925925926
 exported_pipeline = make_pipeline(
-    MinMaxScaler(),
-    FastICA(tol=0.35000000000000003),
-    GradientBoostingClassifier(learning_rate=0.01, max_depth=4, max_features=0.9000000000000001, min_samples_leaf=20, min_samples_split=6, n_estimators=100, subsample=0.4)
+    VarianceThreshold(threshold=0.001),
+    LinearSVC(C=5.0, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
